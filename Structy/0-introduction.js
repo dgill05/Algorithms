573daa71fcf870b2ec15c7e595caf072b5243aef
Daljit Gill <s.gilldaljit@gmail.com>
2022-12-13 15:48:39 -0500

    Max Value complete

---
+++ Structy/0-introduction.js
@@ -0,0 +1,11 @@
+//Max Value
+const maxValue = (nums) => {
+  // todo
+  let max = -Infinity;
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] > max) max = nums[i];
+  }
+  return max;
+};
+
+console.log(maxValue([2,5,-2,10]));
