227382f49ae0844c2aac469dbff17388a33d5686
Daljit Gill <s.gilldaljit@gmail.com>
2022-12-14 16:09:19 -0500

    added isPrime

--- Structy/0-introduction.js
+++ Structy/0-introduction.js
@@ -9,3 +9,16 @@ const maxValue = (nums) => {
 };
 
 console.log(maxValue([2,5,-2,10]));
+
+//----------------------------------------------------------
+// Is prime
+
+const isPrime = (n) => {
+  // todo
+  if (n === 1) return false;
+  if (n === 2) return true;
+  for (let i = 2; i < n; i++){
+    if (n % i === 0) return false;
+  }
+  return true;
+};
