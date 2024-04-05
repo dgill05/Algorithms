df9565de45fca1d3f05f9fc2c84b273ae9a6dfb0
Daljit Gill <s.gilldaljit@gmail.com>
2024-04-05 16:08:08 -0400

    Tribonacci with memoization

--- Structy/1-dynamic.js
+++ Structy/1-dynamic.js
@@ -9,3 +9,17 @@ const fib = (n) => {
 module.exports = {
   fib,
 };
+
+//----------------------------------------------------------
+// Tribonacci
+
+const tribonacci = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+
+  if (n === 0 || n === 1) return 0;
+
+  if (n === 2) return 1;
+
+  memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+  return memo[n];
+};
