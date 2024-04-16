e610ffea49538e089287930b7c15ec58e9b75287
Daljit Gill <s.gilldaljit@gmail.com>
2024-04-16 17:28:13 -0400

    solution overlap subsequence

--- Structy/1-dynamic.js
+++ Structy/1-dynamic.js
@@ -182,5 +182,24 @@ const maxPalinSubsequence = (str, i = 0, j = str.length - 1, memo = {}) => {
     memo[pos] = Math.max(left, right);
     return memo[pos];
   }
+};
 
+//----------------------------------------------------------
+// overlap subsequence
+
+const overlapSubsequence = (str1, str2, i = 0, j = 0, memo = {}) => {
+  // todo
+  const key = i + ',' + j;
+  if (key in memo) return memo[key];
+  if (i === str1.length || j === str2.length) return 0;
+  if (str1[i] === str2[j]){
+    memo[key] = 1 + overlapSubsequence(str1, str2, i + 1, j + 1, memo);
+    return memo[key];
+  } else {
+    memo[key] = Math.max(
+    overlapSubsequence(str1, str2, i + 1, j, memo),
+    overlapSubsequence(str1, str2, i, j + 1, memo)
+    )
+    return memo[key];
+  }
 };
