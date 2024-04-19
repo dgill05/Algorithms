28c745be729d704f050004c09f9e5c8007c1df4a
Daljit Gill <s.gilldaljit@gmail.com>
2024-04-19 16:48:34 -0400

    solution knightly number

--- Structy/1-dynamic.js
+++ Structy/1-dynamic.js
@@ -252,3 +252,45 @@ const quickestConcat_ = (s, words, memo = {}) => {
   memo[s] = min;
   return min;
 };
+
+//----------------------------------------------------------
+// knightly number
+
+const knightlyNumber = (n, m, kr, kc, pr, pc, memo = {}) => {
+  const key = m + ',' + kr + ',' + kc;
+  if (key in memo) {
+    return memo[key];
+  }
+  
+  if (kr < 0 || kr >= n || kc < 0 || kc >= n) {
+    return 0;
+  }
+  
+  if (m === 0) {
+    if (kr === pr && kc === pc) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  
+  const neighbors = [
+    [ kr + 2, kc + 1 ],
+    [ kr - 2, kc + 1 ],
+    [ kr + 2, kc - 1 ],
+    [ kr - 2, kc - 1 ],
+    [ kr + 1, kc + 2 ],
+    [ kr - 1, kc + 2 ],
+    [ kr + 1, kc - 2 ],
+    [ kr - 1, kc - 2 ],
+  ];
+  
+  let count = 0;
+  for (let neighbor of neighbors) {
+    const [neighborRow, neighborCol] = neighbor;
+    count += knightlyNumber(n, m - 1, neighborRow, neighborCol, pr, pc, memo);
+  }
+  
+  memo[key] = count;
+  return count;
+};
