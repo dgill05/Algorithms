a4c1349d0eea1eaf74f1ccd40d31c6521bf55275
Daljit Gill <s.gilldaljit@gmail.com>
2024-04-24 14:50:35 -0400

    solution anagrams

--- Structy/2-array-and-string.js
+++ Structy/2-array-and-string.js
@@ -50,3 +50,34 @@ const compress = (s) => {
   }
   return result.join('');
 };
+
+//----------------------------------------------------------
+// anagrams
+
+const anagrams = (s1, s2) => {
+  // todo
+  const cache = {};
+  for (let char of s1){
+    if (!cache[char]){
+      cache[char] = 1;
+    } else {
+      cache[char]++;
+    }
+  }
+  
+  for (let char of s2){
+    console.log(cache)
+    if(!cache[char]){
+      return false;
+    } else {
+      cache[char]--;
+    }
+    
+  }
+  
+  for (let char in cache){
+    if (cache[char] !== 0) return false;
+  }
+  
+  return true;
+};
