e3dae1afa1d08ab51693767b19ae0c1e9baa7c2a
Daljit Gill <s.gilldaljit@gmail.com>
2024-05-12 13:00:27 -0400

    solution zipper lists

--- Structy/3-linked-list.js
+++ Structy/3-linked-list.js
@@ -63,3 +63,28 @@ const reverseList = (head) => {
   }
   return prev;
 };
+
+//----------------------------------------------------------
+// zipper lists
+
+const zipperLists = (head1, head2) => {
+  // todo
+  let current1 = head1.next;
+  let current2 = head2;
+  let tail = head1;
+  let count = 0;
+  while (current1 && current2) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+    count++;
+  }
+  if (current1) tail.next = current1;
+  if (current2) tail.next = current2;
+  return head1;
+};
