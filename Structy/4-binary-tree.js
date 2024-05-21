c154af8ecc3eb4bc510c9fbb1696daf9ca53ef7f
Daljit Gill <s.gilldaljit@gmail.com>
2024-05-21 15:58:02 -0400

    solution breadth first values

--- Structy/4-binary-tree.js
+++ Structy/4-binary-tree.js
@@ -25,3 +25,21 @@ const depthFirstValues = (root) => {
   }
   return result;
 };
+
+//----------------------------------------------------------
+// breadth first values
+
+const breadthFirstValues = (root) => {
+  // todo
+  if (root === null) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length > 0) {
+    let current = queue.shift();
+    result.push(current.val);
+
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return result;
+};
