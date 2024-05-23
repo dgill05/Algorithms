1469b0713b251c7bcacacbd1b1bf0f1ec75bdde5
Daljit Gill <s.gilldaljit@gmail.com>
2024-05-23 18:06:46 -0400

    solution tree includes

--- Structy/4-binary-tree.js
+++ Structy/4-binary-tree.js
@@ -55,3 +55,13 @@ const treeSum = (root, sum = 0) => {
   return root.val + right + left;
 };
 
+//----------------------------------------------------------
+// tree includes
+
+const treeIncludes = (root, target) => {
+  // todo
+  if (root === null) return false;
+  if (root.val === target) return true;
+
+  return treeIncludes(root.left, target) || treeIncludes(root.right, target);
+};
