b68ef376fa9802fdfe3f5f238f4beaca74d368c6
Daljit Gill <s.gilldaljit@gmail.com>
2024-05-25 12:07:09 -0400

    max root to leaf path sum

--- Structy/4-binary-tree.js
+++ Structy/4-binary-tree.js
@@ -75,3 +75,13 @@ const treeMinValue = (root) => {
 
   return Math.min(root.val, treeMinValue(root.left), treeMinValue(root.right));
 };
+
+//----------------------------------------------------------
+// max root to leaf path sum
+
+const maxPathSum = (root) => {
+  // todo
+  if (root === null) return -Infinity;
+  if (root.left === null && root.right === null) return root.val;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
+};
